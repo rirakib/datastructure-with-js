5e48236bc25a07062842d5728914b81fcfe5d95d
rirakib <rirrakib10@gmail.com>
2023-09-07 12:28:56 -0700

    two pointer middle value

--- twoPointer.js
+++ twoPointer.js
@@ -1,3 +1,5 @@
+//array reverse
+
 function reverseArray(arr) {
 
     let start = 0;
@@ -14,5 +16,23 @@ function reverseArray(arr) {
         end--;
     }
 }
-
 reverseArray([1, 2, 3, 4, 5]); //[5,4,3,2,1]
+
+//midle value find
+
+function middleValueFind(arr) {
+
+    let solo = 0;
+    let fast = solo + 1;
+    let endPoint = arr.length
+    let middle = arr[0]
+
+    while (fast <= endPoint) {
+        middle = arr[solo]
+        solo++;
+        fast += 2
+    }
+
+    return middle
+}
+middleValueFind([1, 2, 3, 4, 5,6,7,8,9]); //5
